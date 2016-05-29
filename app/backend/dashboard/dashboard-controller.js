2fe356ba4c4541fc794e08fbe366ceac8b0a6be6
tchernobyl <ameur.hamdouni@gmail.com>
2016-05-29 19:28:24 +0100

    reformat

--- app/backend/dashboard/dashboard-controller.js
+++ app/backend/dashboard/dashboard-controller.js
@@ -3,7 +3,7 @@ angular.module('backend-module.dashboard2')
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider.state('backend.index', {
             url: '',
-
+            roleUser: 3,
             templateUrl: 'backend/dashboard/dashboard.html',
             controller: 'DashboardController',
             authenticate: true,
